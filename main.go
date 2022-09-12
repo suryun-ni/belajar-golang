75be19f7c651c64c163850eecde7e2eb69239aed
Tengku Surya <82017653+suryun-ni@users.noreply.github.com>
2022-09-12 15:49:13 +0700

    variabel

--- main.go
+++ main.go
@@ -4,6 +4,20 @@ import (
 	"fmt"
 )
 
+// masih awal, A.7-seterusnya
 func main() {
+	//variabel beserta type data
+	// menuliskan type data nya
+	var firstname string = "tengku"
+
+	//tanpa type data
+	lastname := "surya"
+	//atau
+	var lastNasme = "furqan"
+
 	fmt.Println("hello world")
+	//sama kayak println cuman %s digunakan untuk string
+	fmt.Printf("hello %s %s %s!\n", firstname, lastname, lastNasme)
+	// maksud diatas adalah seperti berikut
+	fmt.Println("halo", firstname, lastname, lastNasme+"!")
 }
